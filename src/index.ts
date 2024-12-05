0257151a38077e0e92e0d7542ddffe34696a96f8
rawadhossain <rawad.hossain00@gmail.com>
2024-12-06 01:20:37 +0600

    insert query

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 const pgClient = new Client(
-    'postgresql://neondb_owner:wrWG5KI1ziYB@ep-lucky-snow-a50il0b5.us-east-2.aws.neon.tech/neondb?sslmode=require',
+    'postgresql://neondb_owner:q6XMzT5umsGN@ep-damp-waterfall-a571x77j.us-east-2.aws.neon.tech/neondb?sslmode=require',
 );
 
 pgClient.connect();
@@ -15,13 +15,30 @@ app.post('/signup', async (req, res) => {
     const password = req.body.password;
     const email = req.body.email;
 
+    const city = req.body.city;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode = req.body.pincode;
+
     try {
-        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`;
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;`;
 
         const response = await pgClient.query(insertQuery, [
             username,
             email,
             password,
+        ]);
+
+        const userId = response.rows[0].id;
+
+        const addressInsertQuery = `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5);`;
+
+        const addressInsertresponse = await pgClient.query(addressInsertQuery, [
+            city,
+            country,
+            street,
+            pincode,
+            userId,
         ]);
 
         res.json({
